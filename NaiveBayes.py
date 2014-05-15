b93e10509c0b323d9052eec68325b3c373b0a683
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-05-15 12:03:06 +0530

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -17,7 +17,7 @@ sw = stopwords.words('english')
 #Here we are getting it using nltk
 categoriesFilenameDict={}
 
-#*************NON-GENERALIZED TEXT***********************************
+#*************NON-GENERALIZED CODE***********************************
 
 
 #for category in brown.categories()[:-1]:
@@ -85,7 +85,7 @@ for fileName in trainset:
     fileHandle.close()
 
 ##6) Parse the string to get individual words
-#*************NON-GENERALIZED TEXT***********************************
+#*************NON-GENERALIZED CODE***********************************
     #listWords = re.split(r'\s+',string)
     #listWords = [w[:w.find('/')] for w in listWords if w.find('/')!=-1  and w[w.find('/')+1:]=='jj']
     #listWords = [w.lower() for w in listWords if w.isalnum() and w not in sw]
@@ -129,7 +129,7 @@ for fileName in testset:
     fileHandle.close()
 
 ##10) Similar step, parse the string to get individual words
-#*************NON-GENERALIZED TEXT***********************************
+#*************NON-GENERALIZED CODE***********************************
     #listWords = re.split(r'\s+',string)
     #listWords = [w[:w.find('/')] for w in listWords if w.find('/')!=-1  and w[w.find('/')+1:]=='jj']
     #listWords = [w.lower() for w in listWords if w.isalnum() and w not in sw]
