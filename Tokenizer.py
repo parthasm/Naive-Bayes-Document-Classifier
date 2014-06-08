fb8f0422a42d44c8ce37d73ca09d49c2661fd651
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-06-08 16:48:39 +0530

    Update Tokenizer.py

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,24 +3,19 @@ from nltk.corpus import reuters
 from nltk.corpus import movie_reviews as mr
 import re
 
-sw = stopwords.words('english')
-swd={}
-
-for w in sw:
-    swd[w]=True
-
+sw = set(stopwords.words('english'))
 
 def get_list_tokens_nltk_reuters(fileName):
     string = reuters.raw(fileids=fileName)
     listWords = re.split(r'\W+',string)
-    return [w.lower() for w in listWords if w.isalpha() and len(w)>1 and not swd.get(w.lower(),False)]
+    return [w.lower() for w in listWords if w.isalpha() and len(w)>1 and w.lower() not in sw]
 
 #!!!!!!!!------Possible Improvement: Stemming--------------#
 
 def get_list_tokens_nltk_mr(fileName):
     string = mr.raw(fileids=fileName)
     listWords = re.split(r'\W+',string)
-    return [w.lower() for w in listWords if w.isalpha() and len(w)>2 and not swd.get(w.lower(),False)]
+    return [w.lower() for w in listWords if w.isalpha() and len(w)>2 and w.lower() not in sw]
 #*************NON-GENERALIZED CODE***********************************
     #listWords = re.split(r'\s+',string)
     #listWords = [w[:w.find('/')] for w in listWords if w.find('/')!=-1  and w[w.find('/')+1:]=='jj']
