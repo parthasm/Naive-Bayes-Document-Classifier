b58f5543b8848cbcda2e0d2cab6171603b610457
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-06-08 23:44:07 +0530

    Update FilenameToCat.py

--- FilenameToCat.py
+++ FilenameToCat.py
@@ -4,6 +4,6 @@ from nltk.corpus import movie_reviews as mr
 def reuters_f2c(fileName):
     return reuters.categories(fileids = fileName)[0]
 
-def movie_Reviews_f2c(fileName):
+def movie_reviews_f2c(fileName):
     return mr.categories(fileids = fileName)[0]
 
