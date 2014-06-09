040161ce04a5a363d1f4a511ffecb3b51cc9b3bf
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-06-09 18:04:27 +0530

    Update Bernoulli_NB_nltk_reuters_MI.py
    Changing the function call of mutual information - due to the change in the mutual information formula, CatNumDocs is no longer required as an argument

--- Bernoulli_NB_nltk_reuters_MI.py
+++ Bernoulli_NB_nltk_reuters_MI.py
@@ -40,7 +40,7 @@ trainset = li[0]
 
 #2a) Applying Feature Selection
 
-li = mutual_information(CatNumDocs,trainset,'reuters')
+li = mutual_information(trainset,'reuters')
 WordFeatures = li[0]
 WordList = li[1]
 
