5fdf9fb1d43ecd0042278ada477d01fc2c01e97b
Partha Sarathi Mukherjee <partha.s.m.2.010@gmail.com>
2014-08-12 23:39:05 +0530

    Update Tokenizer.py
    Introducing proper modules in code

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,21 +1,21 @@
 from nltk.corpus import stopwords
-from nltk.corpus import reuters
-from nltk.corpus import movie_reviews as mr
 import re
 
 sw = set(stopwords.words('english'))
 
-def get_list_tokens_nltk_reuters(fileName):
-    string = reuters.raw(fileids=fileName)
+def get_list_tokens_nltk(corpus, fileName):
+    string=''
+    if corpus=='mr':
+        from nltk.corpus import movie_reviews
+        string = movie_reviews.raw(fileids=fileName)
+    else:
+        from nltk.corpus import reuters
+        string = reuters.raw(fileids=fileName)
     listWords = re.split(r'\W+',string)
     return [w.lower() for w in listWords if w.isalpha() and len(w)>1 and w.lower() not in sw]
 
 #!!!!!!!!------Possible Improvement: Stemming--------------#
 
-def get_list_tokens_nltk_mr(fileName):
-    string = mr.raw(fileids=fileName)
-    listWords = re.split(r'\W+',string)
-    return [w.lower() for w in listWords if w.isalpha() and len(w)>2 and w.lower() not in sw]
 #*************NON-GENERALIZED CODE***********************************
     #listWords = re.split(r'\s+',string)
     #listWords = [w[:w.find('/')] for w in listWords if w.find('/')!=-1  and w[w.find('/')+1:]=='jj']
