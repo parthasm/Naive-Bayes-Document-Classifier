fb398eb9b72e39d309ae13aff7e31e98f77515b5
Partha Sarathi Mukherjee <parthasm2010@gmail.com>
2015-01-27 20:42:17 +0530

    Update Tokenizer.py
    converting to lower_case

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,25 +1,25 @@
 from nltk.corpus import stopwords
 import re
 
-sw = set(stopwords.words('english'))
+stopwords_english = set(stopwords.words('english'))
 
-def get_list_tokens_nltk(corpus, fileName):
+def get_list_tokens_nltk(corpus, file_name):
     string=''
     if corpus=='mr':
         from nltk.corpus import movie_reviews
-        string = movie_reviews.raw(fileids=fileName)
+        string = movie_reviews.raw(fileids=file_name)
     else:
         from nltk.corpus import reuters
-        string = reuters.raw(fileids=fileName)
-    listWords = re.split(r'\W+',string)
-    return [w.lower() for w in listWords if w.isalpha() and len(w)>1 and w.lower() not in sw]
+        string = reuters.raw(fileids=file_name)
+    list_words = re.split(r'\W+',string)
+    return [w.lower() for w in list_words if w.isalpha() and len(w)>1 and w.lower() not in stopwords_english]
 
 #!!!!!!!!------Possible Improvement: Stemming--------------#
 
 #*************NON-GENERALIZED CODE***********************************
-    #listWords = re.split(r'\s+',string)
-    #listWords = [w[:w.find('/')] for w in listWords if w.find('/')!=-1  and w[w.find('/')+1:]=='jj']
-    #listWords = [w.lower() for w in listWords if w.isalnum() and w not in sw]
+    #list_words = re.split(r'\s+',string)
+    #list_words = [w[:w.find('/')] for w in list_words if w.find('/')!=-1  and w[w.find('/')+1:]=='jj']
+    #list_words = [w.lower() for w in list_words if w.isalnum() and w not in stopwords_english]
 
     
     #!!!!!!!!------Possible Improvement: Stemming--------------#
