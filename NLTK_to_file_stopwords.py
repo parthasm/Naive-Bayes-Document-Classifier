96ab392c20948bffb1f320eb0e697cd3b145c92a
Partha Sarathi Mukherjee <parthasm2010@gmail.com>
2016-02-06 01:26:46 -0500

    Delete NLTK_to_file_stopwords.py

--- NLTK_to_file_stopwords.py
+++
@@ -1,8 +0,0 @@
-from nltk.corpus import stopwords
-if __name__ == "__main__":
-    stopwords_english = list(set(stopwords.words('english')))
-    file_write_obj_stopwords = open("stopwords.txt","w")
-    for w in stopwords_english:
-        file_write_obj_stopwords.write(w+"\n")
-    file_write_obj_stopwords.close()
-    
